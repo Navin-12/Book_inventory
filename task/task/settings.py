aa5da8d66a41fafe454210ad1d00a161430ba764
Navin-12 <navinbala75@gmail.com>
2021-04-08 17:18:37 +0530

    'firstcommit'

--- task/task/settings.py
+++ task/task/settings.py
@@ -136,6 +136,6 @@ django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+#import dj_database_url 
+#prod_db  =  dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(prod_db)
